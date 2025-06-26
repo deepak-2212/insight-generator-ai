178cca68c7ccc1210b5edad7332855884aaee68f
deepak-2212 <dipakjagtap965@gmail.com>
2025-06-26 18:47:00 +0530

    Add chart view counter and zip download route

--- api_app.py
+++ api_app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, Request, UploadFile, File
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import shutil
 import pandas as pd
 from parser import load_data
 from visualizer import generate_charts
+import os
+import zipfile
+
+# Global chart view counter
+chart_view_count = 0
 
 app = FastAPI()
 
@@ -15,10 +20,17 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "uploaded": False,
+        "chart_info": [],
+        "view_count": chart_view_count
+    })
 
 @app.post("/upload", response_class=HTMLResponse)
 async def upload(request: Request, file: UploadFile = File(...)):
+    global chart_view_count
+
     filepath = f"sample_data/{file.filename}"
     with open(filepath, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -26,8 +38,24 @@ async def upload(request: Request, file: UploadFile = File(...)):
     df = load_data(filepath)
     chart_info = generate_charts(df)
 
+    # Increment the global counter
+    chart_view_count += len(chart_info)
+
     return templates.TemplateResponse("index.html", {
         "request": request,
         "chart_info": chart_info,
-        "uploaded": True
+        "uploaded": True,
+        "view_count": chart_view_count
     })
+
+@app.get("/download_zip")
+def download_zip():
+    zip_filename = "charts.zip"
+    zip_path = f"./{zip_filename}"
+
+    with zipfile.ZipFile(zip_path, 'w') as zipf:
+        for file in os.listdir("outputs"):
+            if file.endswith(".png"):
+                zipf.write(os.path.join("outputs", file), arcname=file)
+
+    return FileResponse(zip_path, filename=zip_filename, media_type='application/zip')
